077093502cb35e655e272349ce8b06f6a7daf494
Artiu <71273256+Artiu@users.noreply.github.com>
2022-10-29 18:26:54 +0200

    Add mic change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
 
 interface UserI {
@@ -19,7 +19,7 @@ function User({ id, micSrcObject }: UserI) {
     return (
         <p>
             {id}
-            <audio ref={audioRef} controls />
+            <audio ref={audioRef} controls autoPlay />
         </p>
     );
 }
@@ -27,27 +27,32 @@ function User({ id, micSrcObject }: UserI) {
 export default function App() {
     const [joinedUsers, setJoinedUsers] = useState<UserI[]>([]);
     const connectionsRef = useRef<Map<string, RTCPeerConnection>>();
-    const micRef = useRef<MediaStream>();
 
-    const getMicrophone = async () => {
-        const mic = await navigator.mediaDevices.getUserMedia({ audio: true });
-        micRef.current = mic;
+    const getMicrophone = async (micId: string) => {
+        const mic = await navigator.mediaDevices.getUserMedia({ audio: { deviceId: micId } });
         return mic;
     };
 
     const createPeerConnection = async (userIdToConnect: string) => {
-        const mic = await getMicrophone();
         const configuration = { iceServers: [{ urls: "stun:openrelay.metered.ca:80" }] };
         const peerConnection = new RTCPeerConnection(configuration);
         connectionsRef.current.set(userIdToConnect, peerConnection);
-        mic.getTracks().forEach((track) => {
-            peerConnection.addTrack(track, mic);
-        });
         let remoteMic: MediaStream;
         peerConnection.addEventListener("track", (event) => {
+            console.log(event);
+
             remoteMic = event.streams[0];
+            setJoinedUsers((users) => {
+                const copy = [...users];
+                const user = copy.find((user) => user.id === userIdToConnect);
+                if (!user) return copy;
+                user.micSrcObject = remoteMic;
+                return copy;
+            });
         });
         peerConnection.addEventListener("connectionstatechange", () => {
+            console.log(peerConnection.connectionState);
+
             if (peerConnection.connectionState === "connected") {
                 setJoinedUsers((users) => [
                     ...users,
@@ -60,6 +65,8 @@ export default function App() {
             }
         });
         peerConnection.addEventListener("icecandidate", (event) => {
+            console.log("ice");
+
             socket.emit("iceCandidate", event.candidate, userIdToConnect);
         });
         return peerConnection;
@@ -93,5 +100,46 @@ export default function App() {
         };
     }, []);
 
-    return joinedUsers.map((user) => <User key={user.id} {...user} />);
+    const [activeMicId, setActiveMicId] = useState<string>();
+    const [microphones, setMicrophones] = useState<MediaDeviceInfo[]>([]);
+
+    const getMicrophones = async () => {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const microphones = devices.filter((device) => device.kind === "audioinput");
+        setMicrophones(microphones);
+        const defaultMic = localStorage.getItem("defaultMic");
+        if (defaultMic) {
+            changeMic(defaultMic);
+            return;
+        }
+        changeMic(microphones[0].deviceId);
+    };
+
+    const changeMic = async (newMicId: string) => {
+        setActiveMicId(newMicId);
+        const newMic = await getMicrophone(newMicId);
+        connectionsRef.current.forEach((conn) => {
+            conn.addTrack(newMic.getAudioTracks()[0], newMic);
+        });
+        localStorage.setItem("defaultMic", newMicId);
+    };
+
+    useEffect(() => {
+        getMicrophones();
+    }, []);
+
+    return (
+        <>
+            <select value={activeMicId} onChange={(e) => changeMic(e.target.value)}>
+                {microphones.map((mic) => (
+                    <option key={mic.deviceId} value={mic.deviceId}>
+                        {mic.label}
+                    </option>
+                ))}
+            </select>
+            {joinedUsers.map((user) => (
+                <User key={user.id} {...user} />
+            ))}
+        </>
+    );
 }
