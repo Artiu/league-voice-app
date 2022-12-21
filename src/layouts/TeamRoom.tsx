9803563550c5bc24e89f3da53520c72d9ddf637b
Artiu <71273256+Artiu@users.noreply.github.com>
2022-12-21 17:27:07 +0100

    Add reconnection after server restart

--- src/layouts/TeamRoom.tsx
+++ src/layouts/TeamRoom.tsx
@@ -2,16 +2,18 @@ import { Button, Container, Flex, Select, Text } from "@chakra-ui/react";
 import JoinedUser from "components/JoinedUser";
 import { useAuthContext } from "contexts/Auth";
 import { useGameStateContext } from "contexts/GameState";
+import { useSocketIOContext } from "contexts/SocketIO";
 import useMic from "hooks/useMic";
 import useWebRTC from "hooks/useWebRTC";
 import { useEffect, useMemo } from "react";
 
 export default function TeamRoom() {
     const { summonerName } = useAuthContext();
+    const { teammates, leaveCall } = useGameStateContext();
+    const socket = useSocketIOContext();
 
     const { microphones, activeMicRef, activeMic, activeMicId, updateMic } = useMic();
     const { joinedUsers, changeAudioForAllPeers } = useWebRTC({ micRef: activeMicRef });
-    const { teammates, leaveCall } = useGameStateContext();
 
     const connectedTeammates = useMemo(
         () =>
@@ -23,6 +25,19 @@ export default function TeamRoom() {
     );
 
     useEffect(() => changeAudioForAllPeers(activeMic), [activeMic]);
+    useEffect(() => {
+        const onReconnection = () => {
+            socket.emit("matchStart");
+        };
+        const onDisconnected = () => {
+            socket.once("connect", onReconnection);
+        };
+        socket.on("disconnect", onDisconnected);
+        return () => {
+            socket.off("disconnect", onDisconnected);
+            socket.off("connect", onReconnection);
+        };
+    }, []);
 
     return (
         <Container maxW="container.xl" width="full">
