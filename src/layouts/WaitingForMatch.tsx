3bf514a08bd3cc87fca45e2847380400119e7d36
Artiu <71273256+Artiu@users.noreply.github.com>
2022-12-26 00:21:16 +0100

    Add connect button to tauri

--- src/layouts/WaitingForMatch.tsx
+++ src/layouts/WaitingForMatch.tsx
@@ -1,9 +1,7 @@
 import { Button, Container, Text } from "@chakra-ui/react";
-import { useAppInfoContext } from "contexts/AppInfo";
 import { useGameStateContext } from "contexts/GameState";
 
 export default function WaitingForMatch() {
-    const { isTauri } = useAppInfoContext();
     const { hasLeftCall, joinCall } = useGameStateContext();
 
     return (
@@ -16,11 +14,9 @@ export default function WaitingForMatch() {
             marginTop="4"
         >
             <Text>{hasLeftCall ? "Match in progress" : "Waiting for match to start..."}</Text>
-            {(!isTauri || hasLeftCall) && (
-                <Button width="max-content" onClick={joinCall}>
-                    {hasLeftCall ? "Reconnect" : "Connect"}
-                </Button>
-            )}
+            <Button width="max-content" onClick={joinCall}>
+                {hasLeftCall ? "Reconnect" : "Connect"}
+            </Button>
         </Container>
     );
 }
