da3135c3f279568621345ec825aca41b7d0199b9
Artiu <oborskiartur123@gmail.com>
2023-11-24 20:09:25 +0100

    Update url

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,23 +2,23 @@ import { Box, Button, Text } from "@chakra-ui/react";
 import { useAppInfoContext } from "contexts/AppInfo";
 
 export default function Footer() {
-    const { isTauri } = useAppInfoContext();
-    if (isTauri) return null;
+	const { isTauri } = useAppInfoContext();
+	if (isTauri) return null;
 
-    return (
-        <Box display="flex" flexDirection="column" justifyContent="center" gap="2" padding="4">
-            <Text fontSize="xl" fontWeight="semibold" textAlign="center">
-                League Voice Official App
-            </Text>
-            <Button
-                marginInline="auto"
-                width="fit-content"
-                as="a"
-                href="https://api.league-voice.site/releases/LeagueVoice.msi"
-                download
-            >
-                Download for Windows
-            </Button>
-        </Box>
-    );
+	return (
+		<Box display="flex" flexDirection="column" justifyContent="center" gap="2" padding="4">
+			<Text fontSize="xl" fontWeight="semibold" textAlign="center">
+				League Voice Official App
+			</Text>
+			<Button
+				marginInline="auto"
+				width="fit-content"
+				as="a"
+				href="https://league-voice-api.artiu.dev/releases/LeagueVoice.msi"
+				download
+			>
+				Download for Windows
+			</Button>
+		</Box>
+	);
 }
